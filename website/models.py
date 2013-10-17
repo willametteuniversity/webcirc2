da93c253e278b68cd6db41ed90cc59f3bd87cfc0
Kameron Schadt <kschadt@willamette.edu>
2013-10-17 14:02:36 -0700

    first model commit

--- website/models.py
+++ website/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model)
+        Name = CharField()
+        ParentID = ForeignKey(Category)
+
+class ItemLabel(models.Model)
+        ItemID = ForeignKey(InventoryItem)
+        CategoryID = ForeignKey(Category)
+
+class InventoryItem(models.Model)
+        AltID = ForeignKey()
+        BrandID = ForeignKey()
+        ModelID = ForeignKey()
+        StorageLocation_ID = ForeignKey()
+        ParentID = ForeignKey(InventoryItem)
+        CategoryID = ForeignKey(Category)
+        Description = CharField()
+        Notes = CharField()
+
