26d61eb4f12dde8a1f251d4c0b5bcc8b6a936a23
Fletcher Haynes <fhaynes@willamette.edu>
2013-10-30 12:03:51 -0700

    Added in bootstrap files, set up basic bootstrap view.

--- website/views.py
+++ website/views.py
@@ -2,6 +2,12 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import render
+
+def index(request):
+    template = loader.get_template('webcirc2.html')
+    context = RequestContext(request, {})
+    return HttpResponse(template.render(context))
+
 def charttest(request):
     template = loader.get_template('charttest.html')
     context = RequestContext(request, {})
