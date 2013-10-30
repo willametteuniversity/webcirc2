79f59ebe882b3b9899571f78a1261983cac38962
Fletcher Haynes <fhaynes@willamette.edu>
2013-10-30 09:30:38 -0700

    Moved class ItemLabel down some, as it references an earlier class.

--- website/models.py
+++ website/models.py
@@ -4,17 +4,18 @@ class Category(models.Model):
         Name = models.CharField()
         ParentID = models.ForeignKey('self')
 
-class ItemLabel(models.Model):
-        ItemID = models.ForeignKey(InventoryItem)
-        CategoryID = models.ForeignKey(Category)
-
 class InventoryItem(models.Model):
         AltID = models.ForeignKey()
         BrandID = models.ForeignKey()
         ModelID = models.ForeignKey()
-        StorageLocation_ID = models.ForeignKey()
+        # TODO: We don't have this defined yet
+        #StorageLocation_ID = models.ForeignKey()
         ParentID = models.ForeignKey('self')
         CategoryID = models.ForeignKey(Category)
         Description = models.CharField()
         Notes = models.CharField()
+
+class ItemLabel(models.Model):
+        ItemID = models.ForeignKey(InventoryItem)
+        CategoryID = models.ForeignKey(Category)
 
