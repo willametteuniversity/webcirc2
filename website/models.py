ea7c230bab92789426215308410b5276f72305a3
Fletcher Haynes <fhaynes@willamette.edu>
2013-10-30 09:33:13 -0700

    Added in placeholders to foreignkeys as some have not been defined yet. Added in max_lengths to charfields as that is required.

--- website/models.py
+++ website/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 class Category(models.Model):
-        Name = models.CharField()
+        Name = models.CharField(max_length=255)
         ParentID = models.ForeignKey('self')
 
 class InventoryItem(models.Model):
-        AltID = models.ForeignKey()
-        BrandID = models.ForeignKey()
-        ModelID = models.ForeignKey()
+        AltID = models.ForeignKey('AltID')
+        BrandID = models.ForeignKey('BrandID')
+        ModelID = models.ForeignKey('ModelID')
         # TODO: We don't have this defined yet
-        #StorageLocation_ID = models.ForeignKey()
+        StorageLocationID = models.ForeignKey('StorageLocationID')
         ParentID = models.ForeignKey('self')
         CategoryID = models.ForeignKey(Category)
-        Description = models.CharField()
-        Notes = models.CharField()
+        Description = models.CharField(max_length=500)
+        Notes = models.CharField(max_length=1000)
 
 class ItemLabel(models.Model):
         ItemID = models.ForeignKey(InventoryItem)
