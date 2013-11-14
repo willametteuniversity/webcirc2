df4f64367be1f5f7f3ea05b055305ed42892b707
kschadt <kschadt@appdev1.willamette.edu>
2013-11-14 13:43:28 -0800

    models.py

--- website/models.py
+++ website/models.py
@@ -1,20 +1,53 @@
 from django.db import models
 
 class Category(models.Model)
-        Name = CharField()
-        ParentID = ForeignKey(Category)
+	Name = CharField()
+	ParentID = ForeignKey(Category)
 
 class ItemLabel(models.Model)
-        ItemID = ForeignKey(InventoryItem)
-        CategoryID = ForeignKey(Category)
+	ItemID = ForeignKey(InventoryItem)
+	CategoryID = ForeignKey(Category)
 
 class InventoryItem(models.Model)
-        AltID = ForeignKey()
-        BrandID = ForeignKey()
-        ModelID = ForeignKey()
-        StorageLocation_ID = ForeignKey()
-        ParentID = ForeignKey(InventoryItem)
-        CategoryID = ForeignKey(Category)
-        Description = CharField()
-        Notes = CharField()
+	AltID = ForeignKey()
+	BrandID = ForeignKey()
+	ModelID = ForeignKey()
+	StorageLocation_ID = ForeignKey()
+	ParentID = ForeignKey(InventoryItem)
+	CategoryID = ForeignKey(Category)
+	Description = CharField()
+	Notes = CharField()
 
+class InventoryWidget(models.Model)
+	
+class Brand(models.Model)
+	Name = CharField()
+
+class ItemModel(models.Model)
+	Description = CharField()
+
+class Building(models.Model)
+	Name = CharField()
+
+class Location(models.Model()
+	BuildingID = ForeignKey(Building)
+	RoomNumber = CharField()
+	Description = CharField()
+
+class Status(models.Model)
+	Description = CharField()
+
+class User(models.Model)
+	Username = CharField()
+
+class Restriction(models.Model)
+	Description = CharField()
+
+class ItemRestrictions(models.Model)
+	ItemID(InventoryItem)
+
+class ItemHistory(models.Model)
+	UserID = ForeignKey(User)
+	ItemID = ForeignKey(InventoryItem)
+	ChangeDesc = CharField()
+	ChangeDateTime = CharField()
