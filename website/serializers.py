b76f777401864cd6aaf8b7693e7ad5fd6c236ead
Kameron Schadt <kschadt@willamette.edu>
2014-01-30 15:02:37 -0800

    Created a Serializer for the Label Model in serializers.py

--- website/serializers.py
+++ website/serializers.py
@@ -1,8 +1,12 @@
 from django.forms import widgets
 from rest_framework import serializers
 from website.models import Collection
-
+from website.models import Label
 
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
-            model = Collection
+        model = Collection
+
+class LabelSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Label
