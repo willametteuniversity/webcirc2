093267e3b3d5e41991b2e833dc3b27259347ee5f
RaysonK <kschadt@willamette.edu>
2014-01-31 11:05:54 -0800

    Updated with patterns for Label functions

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('website.views',
                        url(r'^collections/$', 'collectionList'),
-                       url(r'^collections/(?P<pk>[0-9]+)$', 'collectionDetail'))
+                       url(r'^collections/(?P<pk>[0-9]+)$', 'collectionDetail'),
+                       url(r'^labels/$', 'labelList'),
+                       url(r'^labels/(?P<pk>[0-9]+)$', 'labelDetail'))
 
 urlpatterns = format_suffix_patterns(urlpatterns)
