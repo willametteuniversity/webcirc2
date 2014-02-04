965dd63678fd469cb11bfe079703d8bab9f5de0d
Fletcher Haynes <fhaynes@willamette.edu>
2014-02-03 16:04:16 -0800

    Cleaned up serializers.py.

--- website/serializers.py
+++ website/serializers.py
@@ -1,28 +1,32 @@
 from django.forms import widgets
 from rest_framework import serializers
-from website.models import Collection
-from website.models import Label
+from website.models import *
 
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
 
+
 class LabelSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Label
+    class Meta:
+        model = Label
+
 
 class LabelNotesSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = LabelNotes
-		
+    class Meta:
+        model = LabelNotes
+
+
 class ImageSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Image
-		
+    class Meta:
+        model = Image
+
+
 class StatusSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Status
+    class Meta:
+        model = Status
+
 
 class InventoryItemSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = InventoryItem
+    class Meta:
+        model = InventoryItem
