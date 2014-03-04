6c0e529992360304555eaa5a02e4821047b427d3
Kameron Schadt <kschadt@willamette.edu>
2014-03-04 13:18:30 -0800

    changes to c category hierarchy code

--- website/views.py
+++ website/views.py
@@ -583,11 +583,13 @@ def actionTypeDetail(request, pk):
         current_model.delete()
         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
+
+'''
 @api_view(['GET'])
 def categoryHierarchy(request):
-    '''
-    retrive a list of categories in a hierarchy structure
-    '''
+
+    #retrive a list of categories in a hierarchy structure
+
     if request.method == 'GET':
         root = Label.objects.get(pk=1)
 
@@ -609,3 +611,30 @@ def categoryHierarchy(request):
         #serialized_tree = serializers.serialize('json', Label.objects.all())
 
         return HttpResponse(simplejson.dumps(tree), content_type = 'application/javascript; charset=utf8')
+'''
+
+@api_view(['GET'])
+def categoryHierarchy(request):
+
+    #retrive a list of categories in a hierarchy structure
+
+    if request.method == 'GET':
+        root = Label.objects.get(pk=1)
+
+        def get_nodes(node):
+            print node
+            d = {node.pk : node}
+            children = get_children(node=node)
+            d['key'] = node.pk
+            d['name'] = node.LabelName
+            d['children'] = get_nodes(x for x in children)
+            return d
+
+        def get_children(node):
+            return [x for x in Label.objects.all() if (str(x.pk).startswith(str(node.pk))) and (str(x.pk) != str(node.pk))]
+
+        tree = get_nodes(node=root)
+
+        #serialized_tree = serializers.serialize('json', Label.objects.all())
+
+        return HttpResponse(json.dumps(tree))
