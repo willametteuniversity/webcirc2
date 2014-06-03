7bb51105ba12410cf49e59872884abf4b1e5bac6
Hayden Parker <haydenkparker@gmail.com>
2014-06-03 15:28:02 -0700

    Fixing api urls, cleaning up directories

--- website/urls/actionAPIUrls.py
+++ website/urls/actionAPIUrls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('website.views',
                        url(r'^reservationActions/(?P<pk>[0-9]+)$', 'actionAPIViews.reservationActions'),
-                       url(r'^actions/$', 'views.actionList'),
-                       url(r'^actions/(?P<pk>[0-9]+)$', 'views.actionDetail'),
+                       url(r'^actions/$', 'actionAPIViews.actionList'),
+                       url(r'^actions/(?P<pk>[0-9]+)$', 'actionAPIViews.actionDetail'),
                        )
