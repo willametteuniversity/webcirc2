31430873f4f1c24ddcd76519b6a9f0bc7c851bfc
Hayden Parker <hparker@willamette.edu>
2015-04-10 13:52:51 -0700

    using promises for today's actions

--- static/scripts/models/action.js
+++ static/scripts/models/action.js
@@ -23,25 +23,6 @@ var Action = can.Model({
             });
         }
     },
-//    getEquipment: function(params) {
-//        var deferred = new can.Deferred();
-//        if (params.id) {
-//            $.ajax({
-//                url: '/actionInventoryItems/'+params.id,
-//                type: 'get'
-//            }).then(function(data) {
-//                deferred.resolve(InventoryItem.models(data || []));
-//            }, function(xhr, textStatus, err) {
-//                deferred.reject(err);
-//            });
-//        }
-//        //console.log(deferred);
-//        //return deferred;
-//        deferred.done(function(results){
-//            console.log(results);
-//            return results;
-//        });
-//    },
     create:  'POST /actions/',
     update:  'PUT /actions/{id}',
     destroy: 'DELETE /actions/{id}'
